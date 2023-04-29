bb770380edd1993b45414a0caf11c24e58754b00
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-04-29 18:05:50 +0200

    completed the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = (str, num) => {
+  let strData = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (i = 0; i < num; i++) {
+      strData = strData.concat(str);
+    }
+  }
+  return strData;
 };
 
 // Do not edit below this line
