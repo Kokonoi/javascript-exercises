1e5641e8b8885ccc46f3000d2f2ae8b6688d2c09
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-04-29 19:27:37 +0200

    completed the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = (arr, ...args) => {
+  let mainArr = arr;
+  mainArr = mainArr.filter((x) => !args.includes(x));
+  return mainArr;
 };
 
 // Do not edit below this line
