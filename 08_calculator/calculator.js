febcc6693191dd4c8cfccece8ea4c2028b46b383
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-05 20:16:44 +0200

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let firstNum = a;
+  let secondNum = b;
+  let result = firstNum + secondNum;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let firstNum = a;
+  let secondNum = b;
+  let result = firstNum - secondNum;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (para) {
+  let paraArray = para;
+  let sum = paraArray.reduce((x, y) => x + y, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (para) {
+  let arr = para;
+  return arr.length
+    ? arr.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let answer = 1;
+  if (x == 0 && x == 1) {
+    return 1;
+  } else {
+    for (let i = 1; i <= x; i++) {
+      answer *= i;
+    }
+  }
+  return answer;
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
