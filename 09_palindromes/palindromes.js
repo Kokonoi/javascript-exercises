eb2c84600666dca22fd3ebc61d8631140757c874
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-05 21:49:17 +0200

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = (str) => {
+  let punctuationRegex = /[^a-z]/g;
+  let sentence = str
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .replaceAll(punctuationRegex, "");
+  let sentenceRev = sentence.split("").reverse().join("");
 
+  if (sentence == sentenceRev) {
+    return true;
+  } else {
+    false;
+  }
 };
 
 // Do not edit below this line
