e9403441c4e7c7f84f970d0a2e1c1e4e04f8bfb7
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-06 22:17:30 +0200

    Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let fibo = parseInt(num);
+  let number = 1;
+  let prevNumber = 0;
+  if (fibo === 1) {
+    return number;
+  } else if (fibo < 0) {
+    return "OOPS";
+  } else {
+    for (i = 1; i < fibo; i++) {
+      number += prevNumber;
+      prevNumber = number - prevNumber;
+    }
+    return number;
+  }
 };
 
 // Do not edit below this line
